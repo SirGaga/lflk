252c62716370899a37729eef930e8361c4448270
mengty2019 <mengtianyou376@163.com>
2020-11-18 09:13:47 +0800

    1118暂存

--- lflk-web/src/main/java/com/asideal/lflk/system/controller/RabbitMqMonitorControl.java
+++ lflk-web/src/main/java/com/asideal/lflk/system/controller/RabbitMqMonitorControl.java
@@ -2,14 +2,25 @@ package com.asideal.lflk.system.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
-/*import org.apache.http.HttpEntity;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
+import org.apache.http.HttpEntity;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;*/
+import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,16 +35,26 @@ import java.util.Map;
 @RestController
 @RequestMapping("/system/rabbitMqMonitor")
 @CrossOrigin//是用来处理跨域请求的注解
+@Log4j2
 public class RabbitMqMonitorControl {
     @Resource
     private Environment env;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+    @Value("${spring.rabbitmq.monitorPort}")
+    private String port;
     @GetMapping("/overview")
-    public Map<String, String> overview() {
-        //JSONObject jsonObj = getData("http://192.168.109.121:15672/api/overview", "guest", "guest");
+    @ApiOperation(value = "overview",notes ="overview1")
+    public JSONObject overview() {
+        log.info("host="+env.getProperty("spring.rabbitmq.host"));
+        JSONObject jsonObj = getData("http://"+host+":"+port+"/api/overview", username, password);
         //jsonObj.getString("");
-        return null;
+        return jsonObj;
     }
-/*
 
     public static void main(String[] args) {
         JSONObject jsonObj = new RabbitMqMonitorControl().getData("http://192.168.109.121:15672/api/overview", "guest", "guest");
@@ -69,7 +90,6 @@ public class RabbitMqMonitorControl {
         }
         return jsonObj;
     }
-*/
 
 }
 
